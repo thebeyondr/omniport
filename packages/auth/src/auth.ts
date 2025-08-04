ffcb28ccd99a8860cb6d06cbf4aed0a5d9c6dcf2
Luca Steeb <contact@luca-steeb.com>
2025-08-04 02:44:42 +0200

    fix(auth): add a prefix for session cookies

--- packages/auth/src/auth.ts
+++ packages/auth/src/auth.ts
@@ -68,6 +68,7 @@ export const auth: ReturnType<typeof betterAuth> = betterAuth({
 		defaultCookieAttributes: {
 			domain: cookieDomain,
 		},
+		cookiePrefix: "llmgtwy",
 	},
 	session: {
 		expiresIn: 60 * 60 * 24 * 30, // 30 days
