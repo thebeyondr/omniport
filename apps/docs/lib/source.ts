69d8d030cd93299fa1cb70543b623604f391ff88
smakosh <ismai23l@hotmail.com>
2025-08-25 23:06:25 +0200

    fix: doc source has to return a path

--- apps/docs/lib/source.ts
+++ apps/docs/lib/source.ts
@@ -8,11 +8,13 @@ import { docs } from "@/.source";
 export const source = loader({
 	icon(icon) {
 		if (!icon) {
-			return;
+			return undefined;
 		}
 		if (icon in icons) {
 			return createElement(icons[icon as keyof typeof icons]);
 		}
+
+		return undefined;
 	},
 	baseUrl: "/",
 	source: docs.toFumadocsSource(),
